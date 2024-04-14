3f7036f10ae0f444764e7695525defd372160dcf
Ivan <jingkunyu981007@gmail.com>
2024-04-15 09:26:31 +1000

    working on link
    added list for in-page navigation

--- src/app/Links.js
+++ src/app/Links.js
@@ -4,11 +4,21 @@ export default function Links(params) {
   return (
     <Fragment>
       <ul className="flex gap-4">
-        <li className="textcolor  cursor-pointer">01. <span className="capitalize hover:textsubcolor"> About</span></li>
-        <li>02.<span> Experience</span></li>
-        <li>03.<span> work</span></li>
-        <li>04.<span> contact</span></li>
-        <li>05.<span className="capitalize"> resume</span></li>
+        <li className="transition ease-out cursor-pointer hover:text-yellow-color capitalize">
+        <span className="text-yellow-color mr-2.5">01.</span>about 
+        </li>
+        <li className="transition ease-out cursor-pointer hover:text-yellow-color capitalize">
+          <span className="text-yellow-color mr-2.5"> 02. </span>experience
+        </li>
+        <li className="transition ease-out cursor-pointer hover:text-yellow-color capitalize">
+          <span className="text-yellow-color mr-2.5"> 03.</span>work
+        </li>
+        <li className="transition ease-out cursor-pointer hover:text-yellow-color capitalize">
+          <span className="text-yellow-color mr-2.5"> 04.</span>contact
+        </li>
+        <li className="transition ease-out cursor-pointer hover:text-yellow-color capitalize">
+          <span  className="text-yellow-color mr-2.5">05.</span>resume
+        </li>
       </ul>
     </Fragment>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,14 @@ module.exports = {
       animation: {
         'spin-slow': 'spin 4s linear infinite',
         'spin-slower': 'spin 10s linear infinite normal',
-      }
+      },
+      colors: {
+        'yellow-color': 'rgb(255, 193, 7)',
+        'sub-color':'rgba(225, 232, 235, 1)',
+        'sub-color85':'rgba(225, 232, 235, 0.85)',
+        'sub-color70':'rgba(225, 232, 235, 0.7)',
+
+      },
     },
   },
   plugins: [],
